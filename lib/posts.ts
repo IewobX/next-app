fff3a170d872732856fabde78ca8f284470d71f2
xubowei <xubowei@kkworld.com>
2021-04-22 14:52:54 +0800

    stash

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,61 +1,69 @@
 import fs from "fs";
 import path from "path";
-import remark from 'remark'
-import html from 'remark-html'
-import matter, { GrayMatterFile } from "gray-matter";
+import matter from "gray-matter";
+import remark from "remark";
+import html from "remark-html";
 
-const postsDirectory:string = path.join(process.cwd(), 'posts')
+const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
-    const fileNames:string[] = fs.readdirSync(postsDirectory);
-    const allPostsData:any[] = fileNames.map((fileName:any): any => {
-        const id:string = fileName.replace(/\.md$/, '');
-        const fullPath:string = path.join(postsDirectory, fileName);
-        const fileContents:string = fs.readFileSync(fullPath, 'utf8');
-        const matterResult:GrayMatterFile<string> = matter(fileContents);
+    // Get file names under /posts
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map(fileName => {
+        // Remove ".md" from file name to get id
+        const id = fileName.replace(/\.md$/, "");
 
+        // Read markdown file as string
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf8");
+
+        // Use gray-matter to parse the post metadata section
+        const matterResult = matter(fileContents);
+
+        // Combine the data with the id
         return {
             id,
-            ...matterResult.data
+            ...(matterResult.data as { date: string; title: string })
         };
-    });
-
-    return allPostsData.sort((a:any, b:any): number => {
-        if(a.date < b.date) {
-            return 1
-        } else {
-            return -1
-        }
     })
+    // Sort posts by date
+    return allPostsData.sort((a, b) => {
+        if (a.date < b.date) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
 }
 
 export function getAllPostIds() {
-    const fileNames:string[] = fs.readdirSync(postsDirectory);
-
-    return fileNames.map((fileName:any): any => {
+    const fileNames = fs.readdirSync(postsDirectory);
+    return fileNames.map(fileName => {
         return {
             params: {
-                id: fileName.replace(/\.md$/, '')
+                id: fileName.replace(/\.md$/, "")
             }
-        }
+        };
     });
 }
 
-export async function getPostData(id) {
-    const fullPath: string = path.join(postsDirectory, `${id}.md`);
-    const fileContents: string = fs.readFileSync(fullPath, 'utf-8');
+export async function getPostData(id: string) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    const matterResult:GrayMatterFile<string> = matter(fileContents);
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
 
-    const processedContent: any = await remark()
-    .use(html)
-    .process(matterResult.content);
-
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
     const contentHtml = processedContent.toString();
 
+    // Combine the data with the id and contentHtml
     return {
         id,
         contentHtml,
-        ...matterResult.data
-    }
+        ...(matterResult.data as { date: string; title: string })
+    };
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,57 @@
-import Link from "next/link";
 import Head from "next/head";
-import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
-    const res = await fetch(`https://api.apiopen.top/getJoke?page=1&count=2&type=video`)
-    const data = await res.json();
-
-    return {
-        props: {
-            allPostsData,
-            data
-        }
-    }
+export default function Home({
+    allPostsData
+}: {
+    allPostsData: {
+        date: string
+        title: string
+        id: string
+    }[]
+}) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>这个人很懒，自我介绍什么也没写。</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this in{" "}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                    <Link href={`/posts/${id}`}>
+                        <a>{title}</a>
+                    </Link>
+                    <br />
+                    <small className={utilStyles.lightText}>
+                        <Date dateString={date} />
+                    </small>
+                    </li>
+                ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
 
-export default function Index({ allPostsData, data }) {
-    return (
-        <>
-            <div className="container">
-                <Head>
-                    <title>Index</title>
-                </Head>
-                <div className="index">
-                    <Link href="/posts/first-post">hello world!</Link>
-                    <img src="/image.jpeg" alt="logo" />
-                </div>
-                <section className={utilStyles.headingMd}>...</section>
-                <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                    <h2 className={utilStyles.headingLg}>Blog</h2>
-                    <ul className={utilStyles.list}>
-                        {allPostsData.map(({ id, date, title }) => (
-                            <li className={utilStyles.listItem} key={id}>
-                                <Link href={`/posts/${id}`}>
-                                    <a>{ title }</a>
-                                </Link>
-                                <br />
-                                <small className={utilStyles.lightText}>
-                                    <Date dateString={date}/>
-                                </small>
-                            </li>
-                        ))}
-                    </ul>
-                </section>
-                <p className={utilStyles.p}>{ JSON.stringify(data) }</p>
-            </div>
-            <style jsx>{`
-                .container {
-                    max-width: 750px;
-                    margin: 50px auto;
-                }
-            `}</style>
-        </>
-    )
-};
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    };
+}
