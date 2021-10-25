4064be200a36497d8c20e3467929c51d5a89729c
xubowei <xubowei@kkworld.com>
2021-10-25 14:55:27 +0800

    添加备案号

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,64 +16,67 @@ export default function Layout({
 }) {
     return (
         <div className={styles.container}>
-        <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-                name="description"
-                content="Learn how to build a personal website using Next.js"
-            />
-            <meta
-                property="og:image"
-                content={`https://og-image.vercel.app/${encodeURI(
-                    siteTitle
-                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-            />
-            <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <header className={styles.header}>
-            {home ? (
-            <>
-                <Image
-                priority
-                src="/images/profile.jpeg"
-                className={utilStyles.borderCircle}
-                height={144}
-                width={144}
-                alt={name}
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
                 />
-                <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-            ) : (
-            <>
-                <Link href="/">
-                <a>
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                <>
                     <Image
                     priority
                     src="/images/profile.jpeg"
                     className={utilStyles.borderCircle}
-                    height={108}
-                    width={108}
+                    height={144}
+                    width={144}
                     alt={name}
                     />
-                </a>
-                </Link>
-                <h2 className={utilStyles.headingLg}>
+                    <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                </>
+                ) : (
+                <>
+                    <Link href="/">
+                    <a>
+                        <Image
+                        priority
+                        src="/images/profile.jpeg"
+                        className={utilStyles.borderCircle}
+                        height={108}
+                        width={108}
+                        alt={name}
+                        />
+                    </a>
+                    </Link>
+                    <h2 className={utilStyles.headingLg}>
+                    <Link href="/">
+                        <a className={utilStyles.colorInherit}>{name}</a>
+                    </Link>
+                    </h2>
+                </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
                 <Link href="/">
-                    <a className={utilStyles.colorInherit}>{name}</a>
+                    <a>← Back to home</a>
                 </Link>
-                </h2>
-            </>
+                </div>
             )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-            <div className={styles.backToHome}>
-            <Link href="/">
-                <a>← Back to home</a>
-            </Link>
-            </div>
-        )}
+            <footer className={styles.footer}>
+                <a href={'https://beian.miit.gov.cn/'} target={'_blank'}>京ICP备20027846号</a>
+            </footer>
         </div>
     )
 }
